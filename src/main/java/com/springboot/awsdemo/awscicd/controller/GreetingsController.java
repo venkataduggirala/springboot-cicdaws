d9600cec0a2bca4ec2703d5d3b0ad64366ae20d6
venkataduggirala <srikanthduggirala@gmail.com>
2025-05-06 14:42:35 -0500

    Docker and builspec files

---
+++ src/main/java/com/springboot/awsdemo/awscicd/controller/GreetingsController.java
@@ -0,0 +1,20 @@
+package com.springboot.awsdemo.awscicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/awscicd")
+public class GreetingsController {
+    @GetMapping("/greetings")
+    public String getGreetings(){
+        return "SpringBoot App is deployed on AWS EC2 Instance using CICD";
+    }
+
+    @GetMapping("/health")
+    public String getHealth(){
+        return "UP";
+    }
+
+}
